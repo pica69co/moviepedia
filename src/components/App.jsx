79fb96e4e28500b8c93923f7ea0fffbb9b5815c0
pica69co <oscarwarrieta@gmail.com>
2023-09-29 10:56:59 -0500

    structure-routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import { CssBaseline } from "@mui/material";
-//import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "../pages/home/Home";
+
+import Actors from "./actors/Actors";
+import Profile from "./profile/Profile";
+import MovieInformation from "./movieInformation/MovieInformation";
+import Navbar from "./navbar/Navbar";
+import Movies from "./movies/Movies";
 function App() {
   return (
-    <div className="App">
-      <CssBaseline />
-      <h1>Movie-Pedia</h1>
-    </div>
+    <Router>
+      <main className="App">
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
