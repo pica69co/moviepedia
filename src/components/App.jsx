4f6662eb56211a86c5ebeff01e86fa66a7f14a7f
pica69co <oscarwarrieta@gmail.com>
2023-09-29 11:18:58 -0500

    first styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,31 @@
 import { CssBaseline } from "@mui/material";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
-
 import Actors from "./actors/Actors";
 import Profile from "./profile/Profile";
 import MovieInformation from "./movieInformation/MovieInformation";
 import Navbar from "./navbar/Navbar";
 import Movies from "./movies/Movies";
+
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
   return (
-    <Router>
-      <main className="App">
-        <CssBaseline />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Movies />} />
-          <Route path="/movie/:id" element={<MovieInformation />} />
-          <Route path="/actors/:id" element={<Actors />} />
-          <Route path="/profile:id" element={<Profile />} />
-        </Routes>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile:id" element={<Profile />} />
+          </Routes>
+        </Router>
       </main>
-    </Router>
+    </div>
   );
 }
 
