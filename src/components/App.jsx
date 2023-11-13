5c85c4d0c650a37dde13749c15030cc994329dfa
pica69co <oscarwarrieta@gmail.com>
2023-11-13 17:59:49 -0500

    Add new components and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
 import { CssBaseline } from "@mui/material";
-//import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./Movies/Movies";
+import MovieInformation from "./MovieInformation/MovieInformation";
+import Movies from "./Movies/Movies";
+import Actors from "./Actors/Actors";
+import Profile from "./Profile/Profile";
+import NavBar from "./NavBar/NavBar";
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <CssBaseline />
-      <h1>MoviePedia</h1>
-    </div>
+    <BrowserRouter>
+      <div className={classes.root}>
+        <CssBaseline />
+        <NavBar />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Routes>
+            <>
+              <Route path="/" element={<Movies />} />
+              <Route path="/movie/:id" element={<MovieInformation />} />
+              <Route path="/actors/:id" element={<Actors />} />
+              <Route path="/profile/:id" element={<Profile />} />
+            </>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
