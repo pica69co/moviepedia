ab705a6dc6b3acd5c8bacded3b88e3a59694b567
pica69co <oscarwarrieta@gmail.com>
2023-11-13 19:33:12 -0500

    Add preconnect links and update NavBar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness7,
+  Brightness4,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
+import { useTheme } from "@mui/material/styles";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const theme = useTheme();
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const isAuthenticated = true;
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search..."}
+          <div>
+            {!isAuthenticated ? (
+              <>
+                <Button color="inherit" onClick={() => {}}>
+                  Login &nbsp; <AccountCircle />
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  color="inherit"
+                  component={Link}
+                  to="/profile/:id"
+                  className={classes.linkButton}
+                  onClick={() => {}}
+                >
+                  {!isMobile && <>'My Movies' &nbsp;</>}
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://material-ui.com/static/images/avatar/1.jpg"
+                    style={{ width: "30px", height: "30px" }}
+                    className={classes.avatar}
+                  />
+                </Button>
+              </>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
