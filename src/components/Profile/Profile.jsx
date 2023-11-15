4a071310ba6921279a1b66bcf09fb4dc9632264e
pica69co <oscarwarrieta@gmail.com>
2023-11-15 14:58:47 -0500

    Add logout functionality and display favorite movies section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { userSelector } from "../../features/auth";
+import { Typography, Button, Box } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 const Profile = () => {
-  const { user, isAuthenticated } = useSelector(userSelector);
-  console.log("profile: ", user);
-  return <div style={{ background: "red" }}>Profile</div>;
+  const { user } = useSelector(userSelector);
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+  const favoriteMovies = [];
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile/
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favorites Movies</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
