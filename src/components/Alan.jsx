ca1f15f7fe065e5295ddfb6e5deb8482f002f081
pica69co <oscarwarrieta@gmail.com>
2023-11-16 12:11:18 -0500

    Update Alan SDK key in Alan.jsx

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState, useContext } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { ColorModeContext } from "../utils/ToggleColorMode";
-// const alanKey =
-//  "e9ad67c3f3856e05f786c5a319c0547e2e956eca572e1d8b807a3e2338fdd0dc/stage";
-
+const alanKey = process.env.REACT_APP_ALAN_MOVIEPEDIA_KEY;
+console.log(alanKey);
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
 
   useEffect(() => {
     alanBtn({
-      key: "b50bb8e240149143f786c5a319c0547e2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      key: alanKey,
       onCommand: ({ command, mode }) => {
         if (command === "changeMode") {
           if (mode === "light") {
